acfdc94a9c718365987db1b1ce8df61d7340fbaa
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2022-08-06 15:30:10 +0530

    initial commit

--- server.js
+++ server.js
@@ -20,5 +20,5 @@ app.listen(process.env.PORT || PORT, function(err) {
          console.error(err);
          return;
      }
-     console.log('listening on port' , process.env.port || port);
+     console.log('listening on port' , process.env.port || PORT);
 });
